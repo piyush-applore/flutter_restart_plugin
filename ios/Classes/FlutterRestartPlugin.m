dc68694e6179d9e81eb6308e9c31f5dd6953f870
piyush-applore <87189485+piyush-applore@users.noreply.github.com>
2022-11-09 19:00:51 +0530

    ios restart code

--- ios/Classes/FlutterRestartPlugin.m
+++ ios/Classes/FlutterRestartPlugin.m
@@ -2,6 +2,28 @@
 
 @implementation FlutterRestartPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterRestartPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"flutter_app_restart" binaryMessenger:[registrar messenger]];
+    
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    
+    [self logMessage:@"handle" details:call.method];
+    
+    if ([call.method isEqual:@"restartApp"]) {
+        if ([NSThread isMainThread]) {
+        [self loadBundle];
+    } else {
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            [self loadBundle];
+        });
+    }
+      result("iOS " + UIDevice.current.systemVersion);
+    } else {
+        [self logMessage:@"method not implemented"];
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
